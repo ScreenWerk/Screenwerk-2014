361beb167eba55ed75589f65de19f7914690fa45
argoroots <argo@roots.ee>
2008-10-28 13:04:13 +0000

    Collection kontroller.
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@19 7a690eda-67ec-4556-b061-4c86d960b817

---
+++ www/application/controllers/collection.php
@@ -0,0 +1,65 @@
+<?php
+
+class Collection extends Controller {
+
+	function __construct() {
+		parent::Controller();
+		
+		$this->load->model('Collection_model', 'collection');
+		$this->load->model('Dimension_model', 'dimension');
+		
+		$this->session->protect('schedule');
+		
+		//$this->output->enable_profiler(TRUE);
+	}
+
+
+
+	function index() {
+		$view['data'] = $this->collection->get_list();
+		$view['page_menu_code'] = 'collection';
+		$view['page_submenu'] = array($this->router->class .'/add'=>'Add New '. humanize($this->router->class));
+		$view['page_content'] = $this->load->view('table_view', $view, True);
+		$this->load->view('main_page_view', $view);
+	}
+
+
+
+	function edit($id = NULL) {
+		
+		if($this->input->post('save')) {
+			$this->collection->update();
+			redirect($this->uri->segment(1));
+		}
+
+		if($this->input->post('cancel')) {
+			redirect($this->uri->segment(1));
+		}
+		
+		if($this->input->post('delete')) {
+			$this->collection->delete($this->input->post('id'));
+			redirect($this->uri->segment(1));
+		}
+		
+		$data = $this->collection->get_one($id);
+		
+		$data['dimension']['value'] = $data['dimension_id'];
+		unset($data['dimension_id']);
+		foreach($this->dimension->get_names_list() as $dimension_key => $dimension_value) {
+			$data['dimension']['list'][$dimension_key] = $dimension_value;
+		}
+		
+		$view['data'] = $data;
+		$view['page_menu_code'] = 'schedule';
+		$view['page_content'] = $this->load->view('edit_view', $view, True);
+		$this->load->view('main_page_view', $view);
+	}
+
+
+
+	function add() {
+		$this->edit();
+	}
+
+}
+?>
---
+++ www/application/models/collection_model.php
@@ -0,0 +1,104 @@
+<?php
+
+class Collection_model extends Model {
+
+	function __construct() {
+		parent::Model();
+		$this->load->model('Dimension_model', 'dimension', TRUE);
+	}
+
+
+
+	function get_list() {
+		$this->db->select('id, name, dimension_id');
+		$this->db->from('collections');
+		$this->db->where('customer_id', $_SESSION['user']['customer_id']);
+		$this->db->order_by('name'); 
+		$query = $this->db->get();
+		
+		if($query->num_rows() > 0) {
+			foreach($query->result_array() as $row) {
+				$data[$row['id']] = $row;
+				$data[$row['id']]['dimension'] = $this->dimension->get_name($row['dimension_id']);
+				unset($data[$row['id']]['dimension_id']);
+				unset($data[$row['id']]['id']);
+			}
+		} else {
+			$data = array();
+		}
+
+		return $data;
+	}
+
+
+
+	function get_one($id = NULL) {
+	
+		$this->db->select('id, name, dimension_id');
+		$this->db->from('collections');
+		$this->db->where('customer_id', $_SESSION['user']['customer_id']);
+		$this->db->where('id', $id);
+		$this->db->limit(1);
+		$query = $this->db->get();
+		
+		if($query->num_rows() > 0) {
+			$data = $query->row_array();
+		} else {
+			foreach($query->field_data() as $row) {
+				$data[$row->name] = NULL;
+			}
+		}
+		
+		return $data;
+		
+	}
+
+
+
+	function get_name($id) {
+		$this->db->select('name');
+		$this->db->where('id', $id);
+		$result = $this->db->get('collections');
+		
+		if ($result->num_rows() > 0) return $result->row()->name;
+
+	} 
+
+
+
+	function get_names_list() {
+		foreach($this->get_list() as $key => $value):
+			$result[$key] = $value['name'];
+		endforeach;
+		
+		return $result;
+	} 
+
+
+
+	function delete($id) {
+		$this->db->where('id', $id);
+		$this->db->delete('collections');
+	}
+
+
+
+	function update() {
+		$data = array(
+			'name' => $this->input->post('name'),
+			'dimension_id' => $this->input->post('dimension')
+		);
+		if($this->input->post('id') > 0) {
+			$this->db->where('id', $this->input->post('id'));
+			$this->db->update('collections', $data);
+		} else {
+			$data['customer_id'] = $_SESSION['user']['customer_id'];
+			$this->db->insert('collections', $data);
+		}
+	}
+
+
+
+}
+
+?>
