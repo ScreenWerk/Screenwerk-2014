0c1cad351cbf63f910e73af369073da04ff8da7c
mitselek <mihkel.putrinsh@gmail.com>
2008-10-29 14:31:12 +0000

    http://codeigniter.com/wiki/Automatic_base_url/
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@33 7a690eda-67ec-4556-b061-4c86d960b817

--- www/application/config/config.php
+++ www/application/config/config.php
@@ -11,7 +11,11 @@
 |	http://www.your-site.com/
 |
 */
-$config['base_url']	= "http://screenwerk.arx.ee/";
+$root = "http://".$_SERVER['HTTP_HOST'];
+$root .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+
+$config['base_url']    = "$root";
+#$config['base_url']	= "http://screenwerk.arx.ee/";
 
 /*
 |--------------------------------------------------------------------------
