4c42daa6215a93f3dcce6cf08089c5fc5830a289
argoroots <argo@roots.ee>
2008-11-03 13:04:39 +0000

    m2m jaoks vajalik muudatus edit_view's.
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@48 7a690eda-67ec-4556-b061-4c86d960b817

--- www/application/views/edit_view.php
+++ www/application/views/edit_view.php
@@ -1,7 +1,7 @@
 <h2><?= humanize($this->router->method .'_'. $this->router->class); ?></h2>
 
 <?php
-	echo form_open(current_url());
+	echo form_open(current_url(), array('style'=>'margin-bottom:20px;'));
 	
 	foreach($data as $field_name => $field_value):
 	
@@ -27,3 +27,30 @@
 </div>
 
 <?= form_close(); ?>
+
+
+
+<?php if(isset($data_m2m)) foreach($data_m2m as $m2m_id => $m2m_value): ?>
+
+<h2><?= humanize($this->router->class .'_'. plural($m2m_id)); ?></h2>
+
+<table class="data_table" cellspacing="0" cellpadding="0" style="margin-bottom:20px;">
+
+<?php 
+	$header_set = FALSE;
+	foreach($m2m_value as $row_id => $row_value):
+		if(!$header_set) {
+			echo '<tr><th>'. implode('</th><th>', array_keys($row_value)) .'</th><th></th></tr>';
+			$header_set=TRUE;
+		}
+?>
+
+	<tr>
+		<td><?= implode('</td><td>', array_values($row_value)); ?></td>
+	</tr>
+
+<?php endforeach; ?>
+
+</table>
+
+<?php endforeach; ?>
