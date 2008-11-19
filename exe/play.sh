7a7062b7575c4f694b006103f520244aca82166e
mitselek <mihkel.putrinsh@gmail.com>
2008-11-19 10:12:52 +0000

    Resolves #48. Player tapab aegunud meediaprotsessid
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@59 7a690eda-67ec-4556-b061-4c86d960b817

--- exe/play.sh
+++ exe/play.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-mount.vboxsf SWSHARE /mnt/swshare/
+. /root/screen.conf
 
-SCREEN_ID=29
-MONITORASPECT="1280/800"
+#SCREEN_ID=29
+MONITORASPECT="1440/900"
 TODAY=`date +'%Y%m%d'`
+LOGFILE=${SCREEN_ID}_player.log
+echo "" > ${LOGFILE}
 
+# exec dwm &
+# iceweasel -geometry=720x450-0-0 google.com &
+#iceweasel --geometry=720x450+0+0 google.com &
+
+
+date +'%c starting up' >> ${LOGFILE}
 
 cat /mnt/swshare/${SCREEN_ID}_${TODAY}.events | \
 while read event
@@ -14,37 +22,57 @@ do
 
   #Continue, if event is Stop event
   event_name=${event_a[1]}
+  event_id=${event_a[2]}
   if [ "${event_name}" = "stop" ]
   then
+    if [ ${event_pid_a[${event_id}]} ]
+    then
+      if [ `ps -ef | grep ${event_pid_a[${event_id}]} | wc -l` -eq 1 ]
+      then
+        echo "kill ${event_pid_a[${event_id}]}" | tee -a ${LOGFILE}
+        kill ${event_pid_a[${event_id}]}
+      fi
+    fi
     continue
   fi
 
   # Continue, if stop time in past.
-  stop_time=${event_a[6]}
+  stop_time=${event_a[9]}
   if [ `echo ${stop_time}|cut -c1,2,4,5,7,8` -lt `date +'%H%M%S'` ]
   then
+    echo " >< stop time passed: ${stop_time}"
+    date +"%c stop time passed ${stop_time}" >> ${LOGFILE}
     continue
   fi
 
-
   start_time=${event_a[0]}
-  event_id=${event_a[2]}
   media_id=${event_a[3]}
   media_type=${event_a[4]}
-  geometry=${event_a[5]}
-  XxY=`echo ${geometry} | cut -d"+" -f1`
+  W=${event_a[5]}
+  H=${event_a[6]}
+  X=${event_a[7]}
+  Y=${event_a[8]}
 
   # Play right now, if start time in past.
   if [ `echo ${start_time}|cut -c1,2,4,5,7,8` -lt `date +'%H%M%S'` ]
   then
-    date >> /mnt/swshare/${SCREEN_ID}_xx.log
-    echo $event >> /mnt/swshare/${SCREEN_ID}_xx.log
     /usr/bin/mplayer \
       -monitoraspect ${MONITORASPECT} \
-      -geometry $geometry \
+      -geometry ${W}x${H}+${X}+${Y} \
       -loop 1 \
-      /mnt/swshare/${SCREEN_ID}_${media_id}_${XxY}.video \
+      /mnt/swshare/${SCREEN_ID}_${media_id}_${W}x${H}.video \
       2>&1 >/dev/null &
+    event_pid_a[${event_id}]=$!
+      echo " >< play"
+      date +"%c
+    /usr/bin/mplayer \
+      -monitoraspect ${MONITORASPECT} \
+      -geometry ${W}x${H}+${X}+${Y} \
+      -loop 1 \
+      /mnt/swshare/${SCREEN_ID}_${media_id}_${W}x${H}.video \
+      2>&1 >/dev/null &" >> ${LOGFILE}
+    echo "events: ${event_pid_a[@]}" | tee -a ${LOGFILE}
+      
     continue
   fi
 
@@ -53,22 +81,32 @@ do
   d_H="$((10#`echo $start_time|cut -c1-2`-10#`date +'%H'`))"
   d_M="$((10#`echo $start_time|cut -c4-5`-10#`date +'%M'`))"
   d_S="$((10#`echo $start_time|cut -c7-8`-10#`date +'%S'`))"
-#  d_NS=$((10#`echo $start_time|cut -c10-11`-10#`date --rfc-3339=ns|cut -d. -f2|cut -c1-2`))
-  echo ${start_time} - `date --rfc-3339=ns` >> /mnt/swshare/${SCREEN_ID}
+  echo -n " - ${start_time} - "`date --rfc-3339=ns`
 
   delay=$(($d_H*3600+$d_M*60+$d_S+2))
   sleep $delay
 
-  date >> /mnt/swshare/${SCREEN_ID}_xx.log
-  echo $event >> /mnt/swshare/${SCREEN_ID}_xx.log
+  date +' - %c >< play'
   /usr/bin/mplayer \
     -monitoraspect ${MONITORASPECT} \
-    -geometry $geometry \
+    -geometry ${W}x${H}+${X}+${Y} \
     -loop 1 \
-    /mnt/swshare/${SCREEN_ID}_${media_id}_${XxY}.video \
+    /mnt/swshare/${SCREEN_ID}_${media_id}_${W}x${H}.video \
     2>&1 >/dev/null &
+    event_pid_a[${event_id}]=$!
+      echo " >< play"
+      date +"%c
+    /usr/bin/mplayer \
+      -monitoraspect ${MONITORASPECT} \
+      -geometry ${W}x${H}+${X}+${Y} \
+      -loop 1 \
+      /mnt/swshare/${SCREEN_ID}_${media_id}_${W}x${H}.video \
+      2>&1 >/dev/null &" >> ${LOGFILE}
+    echo "events: ${event_pid_a[@]}" | tee -a ${LOGFILE}
 
-done
+done # >> /mnt/swshare/${SCREEN_ID}_events.log # | dwm
+
+# . ${0} # uncomment this, if You like to restart the player
 
 exit 0
 
