6b17791f662cd499885159e967e63794cac6a71d
mitselek <mihkel.putrinsh@gmail.com>
2008-11-30 06:36:20 +0000

    resolves #52. ping.sh on nüüd targem ja ilusam
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@72 7a690eda-67ec-4556-b061-4c86d960b817

--- exe/screen/ping.sh
+++ exe/screen/ping.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
 
-. /root/screen.conf
+if [ -r /root/screen.conf ]
+then
+   . /root/screen.conf
+else
+   SCREEN_ID=NONAME
+fi
 
 TODAY=`date +'%Y%m%d'`
 LOGFILE=ping_${SCREEN_ID}_${TODAY}.log
-echo "" > ${LOGFILE}
 
-while [ 1 ]
-do
-
-   ssh remotes@moos.ww.ee \
-    "date +\"%c %z|${SID}|%s-`date +'%s|%c %z'`|ping\" | tee -a ${LOGFILE}" \
-    | tee -a ${LOGFILE} | cut -d"|" -f3 | bc
+ssh remotes@moos.ww.ee \
+ "date +\"%c %z|${SCREEN_ID}|%s-`date +'%s|%c %z'`|ping\" | tee -a ${LOGFILE}" \
+ | tee -a ${LOGFILE} >> /dev/null # | cut -d"|" -f3 | bc >> ${LOGFILE}
 
 
-   if [ `ssh remotes@moos.ww.ee "ls -a1 signals/ | grep -e ^${SID}\$ -c"` -eq 1 ]
-   then
-      scp remotes@moos.ww.ee:signals/${SCREEN_ID} . >> ${LOGFILE}
-      ssh remotes@moos.ww.ee "date +\"%c %z|${SID}|`date +'%c %z'`|match\" >> ${LOGFILE}" >> ${LOGFILE}
-      . ${SCREEN_ID}
-   fi
+if [ `ssh remotes@moos.ww.ee "ls -a1 signals/ | grep -e ^${SID}\$ -c"` -eq 1 ]
+then
+   scp remotes@moos.ww.ee:signals/${SCREEN_ID} . >> ${LOGFILE}
+   ssh remotes@moos.ww.ee "date +\"%c %z|${SID}|`date +'%c %z'`|match\" >> ${LOGFILE}" >> ${LOGFILE}
+   . ${SCREEN_ID}
+fi
 
-   sleep 80
+sleep 23
 
-done
+# fork to new process -
+#this way modifications to script will take immediate effect
+$0&
 
 exit 0
