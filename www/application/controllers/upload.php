f4fc042cbd4b48db339ef00d4075df0ad6a5cad8
argoroots <argo@roots.ee>
2009-01-08 20:13:23 +0000

    Veidi parem häkk suvalise failitüübi lubamiseks kui CI funktsioone muuta.
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@82 7a690eda-67ec-4556-b061-4c86d960b817

--- www/application/controllers/upload.php
+++ www/application/controllers/upload.php
@@ -20,11 +20,14 @@ class Upload extends Controller {
 
 
 	function do_upload() {
+	
+		global $mimes;
+		if (count($mimes) == 0) @require_once(APPPATH.'config/mimes'.EXT);
 		
 		$config['upload_path'] = '../ftp/incoming/';
-		$config['allowed_types'] = 'image|video|text';
+		$config['allowed_types'] = $this->__array_search_recursive($_FILES['upload_file']['type'], $mimes);
 		$config['remove_spaces'] = TRUE;
-		
+
 		$this->load->library('upload', $config);
 	
 		if ( ! $this->upload->do_upload('upload_file')) {
@@ -39,6 +42,12 @@ class Upload extends Controller {
 
 	}	
 	
-}
+	function __array_search_recursive($needle, $haystack) {     
+    foreach ($haystack as $k => $v) {
+      for ($i=0; $i<count($v); $i++)
+        if ($v[$i] === $needle) return $k;
+    	}
+  	}
+	}
 
 ?>
