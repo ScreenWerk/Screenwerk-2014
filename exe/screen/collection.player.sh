17db5b416bc05759c4d511ab331e5ae96aa7d8c0
mitselek <mihkel.putrinsh@gmail.com>
2009-01-23 17:35:31 +0000

    re #49: schedule.player refactor
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@95 7a690eda-67ec-4556-b061-4c86d960b817

---
+++ exe/screen/collection.player.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+WORK_DIR=`dirname ${0}`
+. ${WORK_DIR}/screen.conf
+
+COLLECTION_FILE=${MEDIA_DIR}/${1}.collection
+
+#echo ${COLLECTION_FILE}
+
+while [ "1" -eq "1" ]
+do
+  :
+  sleep 1
+done
+
+exit
--- exe/screen/schedule.player.sh
+++ exe/screen/schedule.player.sh
@@ -42,7 +42,8 @@ L=$((J/11))
 MON=$((J+2-12*L))
 YR=$((100*(N-49)+I+L))
 
-printf '%04i %02i %02i\n' $YR $MON $DAY
+echo $YR $MON $DAY
+#printf '%04i %02i %02i\n' $YR $MON $DAY
 }
 
 
@@ -120,196 +121,255 @@ function min()
 
 function last_event()
 {
-   I=0
+   LAST_EVENT=( "ID" 0 0 0 )
+   NOW_DATE=`date +'%-Y %-m %-d'`  # 2009 1 18
+   NOW_JD=`get_astro_JD $NOW_DATE`
+   NOW_TIME_H=$((`date +'%-H'`))  # 23
+   NOW_TIME_M=$((`date +'%-M'`))  # 11
 
-   while [ "$I" -le "366" ]
+
+   firstline=1
+
+   while read l
    do
-      I_JD=$((NOW_JD-I))
-      I=$((I+1))
-      [[ "$I_JD" -lt "${cronline[8]}" ]] && return 0
-      [[ "$I_JD" -lt "${LAST_EVENT[1]}" ]] && return 0
-
-      I_DATE=`get_greg_from_JD $I_JD`
-      echo $I_DATE
-      I_DAY=`echo $I_DATE | cut -d' ' -f3`
-      I_MONTH=`echo $I_DATE | cut -d' ' -f2`
-      I_WEEKDAY=$((I_JD-I_JD/7*7+1));
-
-      if [ ! "${cronline[3]}" = "*" ]; then
-         in_array ${I_DAY} ${cronline[3]}
-         [[ $? -eq 0 ]] && continue
+      if [ $firstline -eq 1 ]
+      then
+         firstline=0
+         continue
       fi
-      if [ ! "${cronline[4]}" = "*" ]; then
-         in_array ${I_MONTH} ${cronline[4]}
-         [[ $? -eq 0 ]] && continue
-      fi
-      if [ ! "${cronline[5]}" = "*" ]; then
-         in_array ${I_WEEKDAY} ${cronline[5]}
-         [[ $? -eq 0 ]] && continue
-      fi
+      
+      cronline=( $l ) # split $l by semicolon
 
-      #echo "= Candidate last JDay: ${I_JD}"
-      #  At this point we have found a candidate day for latest event
-      if [ "${I_JD}" -eq "${NOW_JD}" ]; then
-         if [ "${cronline[2]}" = "*" ]; then
-            EVENT_H=${NOW_TIME_H}
-         else
-            #echo "=is_ge ${NOW_TIME_H} ${cronline[2]}="
-            is_ge ${NOW_TIME_H} ${cronline[2]}
-            [[ $? -eq 0 ]] && continue
-            EVENT_H=`get_ge ${NOW_TIME_H} ${cronline[2]}`
-         fi
-      else
-         if [ "${cronline[2]}" = "*" ]; then
-            EVENT_H=23
-         else
-            EVENT_H=`max ${cronline[2]}`
-         fi
-      fi
+      SIFS=$IFS; IFS='-'
+         cronline[8]=`get_astro_JD ${cronline[6]}`
+         cronline[9]=`get_astro_JD ${cronline[7]}`
+      IFS=$SIFS; 
 
+      PIFS=$IFS; IFS=' ,'
 
-      if [ "${I_JD}" -eq "${NOW_JD}" -a "${EVENT_H}" -eq "${NOW_TIME_H}" ]; then
-         if [ "${cronline[1]}" = "*" ]; then
-            EVENT_M=${NOW_TIME_M}
-         else
-            #echo "= is_ge ${NOW_TIME_M} ${cronline[1]}="
-            is_ge ${NOW_TIME_M} ${cronline[1]}
-            if [ $? -eq 0 ]; then
-               #echo "= NO ="
-               [[ "${NOW_TIME_H}" -eq "0" ]] && continue
+         I=0
+         while [ "$I" -le "366" ]
+         do
+            I_JD=$((NOW_JD-I))
+            I=$((I+1))
+            [[ "$I_JD" -lt "${cronline[8]}" ]] && break 1
+            [[ "$I_JD" -lt "${LAST_EVENT[1]}" ]] && break 1
+
+            I_DATE=`get_greg_from_JD $I_JD`
+            I_DAY=`echo $I_DATE | cut -d' ' -f3`
+            I_MONTH=`echo $I_DATE | cut -d' ' -f2`
+            I_WEEKDAY=$((I_JD-I_JD/7*7+1));
+
+            if [ ! "${cronline[3]}" = "*" ]; then
+               in_array ${I_DAY} ${cronline[3]}
+               [[ $? -eq 0 ]] && continue
+            fi
+            if [ ! "${cronline[4]}" = "*" ]; then
+               in_array ${I_MONTH} ${cronline[4]}
+               [[ $? -eq 0 ]] && continue
+            fi
+            if [ ! "${cronline[5]}" = "*" ]; then
+               in_array ${I_WEEKDAY} ${cronline[5]}
+               [[ $? -eq 0 ]] && continue
+            fi
+
+            #echo "= Candidate last JDay: ${I_JD}"
+            #  At this point we have found a candidate day for latest event
+            
+            if [ "${I_JD}" -eq "${NOW_JD}" ]; then
                if [ "${cronline[2]}" = "*" ]; then
-                  EVENT_H=$((NOW_TIME_H-1))
+                  EVENT_H=${NOW_TIME_H}
                else
-                  #echo "=is_ge $((NOW_TIME_H-1)) ${cronline[2]}="
-                  is_ge $((NOW_TIME_H-1)) ${cronline[2]}
+                  is_ge ${NOW_TIME_H} ${cronline[2]}
                   [[ $? -eq 0 ]] && continue
-                  EVENT_H=`get_ge $((NOW_TIME_H-1)) ${cronline[2]}`
+                  EVENT_H=`get_ge ${NOW_TIME_H} ${cronline[2]}`
                fi
-               EVENT_M=`max ${cronline[1]}`
             else
-               EVENT_M=`get_ge ${NOW_TIME_M} ${cronline[1]}`
-               #echo "= YES : ${EVENT_M}="
+               if [ "${cronline[2]}" = "*" ]; then
+                  EVENT_H=23
+               else
+                  EVENT_H=`max ${cronline[2]}`
+               fi
             fi
-         fi
-      else
-         if [ "${cronline[1]}" = "*" ]; then
-            EVENT_M=59
-         else
-            EVENT_M=`max ${cronline[1]}`
-         fi
-      fi
 
-      if [ "${I_JD}" -lt "${LAST_EVENT[1]}" ]; then
-         echo br1; break
-      elif [ "${I_JD}" -eq "${LAST_EVENT[1]}" ]; then
-         if [ "${EVENT_H}" -lt "${LAST_EVENT[2]}" ]; then
-            echo br2; break
-         elif [ "${EVENT_H}" -eq "${LAST_EVENT[2]}" ]; then
-            if [ "${EVENT_M}" -lt "${LAST_EVENT[3]}" ]; then
-               echo br3; break
+
+            if [ "${I_JD}" -eq "${NOW_JD}" -a "${EVENT_H}" -eq "${NOW_TIME_H}" ]; then
+               if [ "${cronline[1]}" = "*" ]; then
+                  EVENT_M=${NOW_TIME_M}
+               else
+                  is_ge ${NOW_TIME_M} ${cronline[1]}
+                  if [ $? -eq 0 ]; then
+                     [[ "${NOW_TIME_H}" -eq "0" ]] && continue
+                     if [ "${cronline[2]}" = "*" ]; then
+                        EVENT_H=$((NOW_TIME_H-1))
+                     else
+                        is_ge $((NOW_TIME_H-1)) ${cronline[2]}
+                        [[ $? -eq 0 ]] && continue
+                        EVENT_H=`get_ge $((NOW_TIME_H-1)) ${cronline[2]}`
+                     fi
+                     EVENT_M=`max ${cronline[1]}`
+                  else
+                     EVENT_M=`get_ge ${NOW_TIME_M} ${cronline[1]}`
+                  fi
+               fi
+            else
+               if [ "${cronline[1]}" = "*" ]; then
+                  EVENT_M=59
+               else
+                  EVENT_M=`max ${cronline[1]}`
+               fi
             fi
-         fi
-      fi
-      LAST_EVENT=( ${cronline[0]} ${I_JD} ${EVENT_H} ${EVENT_M} )
 
-      echo br4; break
+            if [ "${I_JD}" -lt "${LAST_EVENT[1]}" ]; then
+               break 1
+            elif [ "${I_JD}" -eq "${LAST_EVENT[1]}" ]; then
+               if [ "${EVENT_H}" -lt "${LAST_EVENT[2]}" ]; then
+                  break 1
+               elif [ "${EVENT_H}" -eq "${LAST_EVENT[2]}" ]; then
+                  if [ "${EVENT_M}" -lt "${LAST_EVENT[3]}" ]; then
+                     break 1
+                  fi
+               fi
+            fi
+            LAST_EVENT=( ${cronline[0]} ${I_JD} ${EVENT_H} ${EVENT_M} )
+
+            break 1
+         done
+
+      IFS=$PIFS
+      
    done
+   
 }
 
 function next_event()
 {
-   J=${I}
-   I=0
+   NEXT_EVENT=( "ID" 3999999 23 59 )
+   NOW_DATE=`date +'%-Y %-m %-d'`  # 2009 1 18
+   NOW_JD=`get_astro_JD $NOW_DATE`
+   NOW_TIME_H=$((`date +'%-H'`))  # 23
+   NOW_TIME_M=$((`date +'%-M'`))  # 11
+
+   NOW_TIME_M=$((NOW_TIME_M+1))
+   if [ "${NOW_TIME_M}" -eq "60" ]; then
+      NOW_TIME_M=0
+      NOW_TIME_H=$((NOW_TIME_H+1))
+      if [ "${NOW_TIME_H}" -eq "24" ]; then
+         NOW_TIME_H=0
+         NOW_JD=$((NOW_JD+1))
+      fi
+   fi
 
 
-   while [ "$J" -le "366" ]
+   firstline=1
+
+   while read l
    do
-      I_JD=$((NOW_JD+I))
-      I=$((I+1))
-      J=$((J+1))
-      [[ "$I_JD" -gt "${cronline[9]}" ]] && return 0
-      [[ "$I_JD" -gt "${NEXT_EVENT[1]}" ]] && return 0
-
-      I_DATE=`get_greg_from_JD $I_JD`
-      I_DAY=`echo $I_DATE | cut -d' ' -f3`
-      I_MONTH=`echo $I_DATE | cut -d' ' -f2`
-      I_WEEKDAY=$((I_JD-I_JD/7*7+1));
-
-      if [ ! "${cronline[3]}" = "*" ]; then
-         in_array ${I_DAY} ${cronline[3]}
-         [[ $? -eq 0 ]] && continue
-      fi
-      if [ ! "${cronline[4]}" = "*" ]; then
-         in_array ${I_MONTH} ${cronline[4]}
-         [[ $? -eq 0 ]] && continue
-      fi
-      if [ ! "${cronline[5]}" = "*" ]; then
-         in_array ${I_WEEKDAY} ${cronline[5]}
-         [[ $? -eq 0 ]] && continue
+      if [ $firstline -eq 1 ]
+      then
+         firstline=0
+         continue
       fi
 
-      #echo "= Candidate next JDay: ${I_JD}"
-      #  At this point we have found a candidate day for next event
-      if [ "${I_JD}" -eq "${NOW_JD}" ]; then
-         if [ "${cronline[2]}" = "*" ]; then
-            EVENT_H=${NOW_TIME_H}
-         else
-            #echo "=is_le ${NOW_TIME_H} ${cronline[2]}="
-            is_le ${NOW_TIME_H} ${cronline[2]}
-            [[ $? -eq 0 ]] && continue
-            EVENT_H=`get_le ${NOW_TIME_H} ${cronline[2]}`
-         fi
-      else
-         if [ "${cronline[2]}" = "*" ]; then
-            EVENT_H=0
-         else
-            EVENT_H=`min ${cronline[2]}`
-         fi
-      fi
+      cronline=( $l ) # split $l by semicolon
 
+      SIFS=$IFS; IFS='-'
+         cronline[8]=`get_astro_JD ${cronline[6]}`
+         cronline[9]=`get_astro_JD ${cronline[7]}`
+      IFS=$SIFS; 
 
-      if [ "${I_JD}" -eq "${NOW_JD}" -a "${EVENT_H}" -eq "${NOW_TIME_H}" ]; then
-         if [ "${cronline[1]}" = "*" ]; then
-            EVENT_M=${NOW_TIME_M}
-         else
-            #echo "=is_le $((NOW_TIME_M+1)) ${cronline[1]}="
-            is_le $((NOW_TIME_M+1)) ${cronline[1]}
-            if [ $? -eq 0 ]; then
-               [[ "${NOW_TIME_H}" -eq "23" ]] && continue
+      PIFS=$IFS; IFS=' ,'
+
+         I=0
+         while [ "$I" -le "366" ]
+         do
+            I_JD=$((NOW_JD+I))
+            I=$((I+1))
+            [[ "$I_JD" -gt "${cronline[9]}" ]] && break 1
+            [[ "$I_JD" -gt "${NEXT_EVENT[1]}" ]] && break 1
+
+            I_DATE=`get_greg_from_JD $I_JD`
+            I_DAY=`echo $I_DATE | cut -d' ' -f3`
+            I_MONTH=`echo $I_DATE | cut -d' ' -f2`
+            I_WEEKDAY=$((I_JD-I_JD/7*7+1));
+
+            if [ ! "${cronline[3]}" = "*" ]; then
+               in_array ${I_DAY} ${cronline[3]}
+               [[ $? -eq 0 ]] && continue
+            fi
+            if [ ! "${cronline[4]}" = "*" ]; then
+               in_array ${I_MONTH} ${cronline[4]}
+               [[ $? -eq 0 ]] && continue
+            fi
+            if [ ! "${cronline[5]}" = "*" ]; then
+               in_array ${I_WEEKDAY} ${cronline[5]}
+               [[ $? -eq 0 ]] && continue
+            fi
+
+            #echo "= Candidate next JDay: ${I_JD}"
+            #  At this point we have found a candidate day for next event
+            
+            if [ "${I_JD}" -eq "${NOW_JD}" ]; then
                if [ "${cronline[2]}" = "*" ]; then
-                  EVENT_H=$((NOW_TIME_H+1))
+                  EVENT_H=${NOW_TIME_H}
                else
-                  is_le $((NOW_TIME_H+1)) ${cronline[2]}
+                  is_le ${NOW_TIME_H} ${cronline[2]}
                   [[ $? -eq 0 ]] && continue
-                  EVENT_H=`get_le $((NOW_TIME_H+1)) ${cronline[2]}`
+                  EVENT_H=`get_le ${NOW_TIME_H} ${cronline[2]}`
                fi
-               EVENT_M=`min ${cronline[1]}`
             else
-               EVENT_M=`get_le $((NOW_TIME_M+1)) ${cronline[1]}`
+               if [ "${cronline[2]}" = "*" ]; then
+                  EVENT_H=0
+               else
+                  EVENT_H=`min ${cronline[2]}`
+               fi
             fi
-         fi
-      else
-         if [ "${cronline[1]}" = "*" ]; then
-            EVENT_M=0
-         else
-            EVENT_M=`min ${cronline[1]}`
-         fi
-      fi
-      
-      if [ "${I_JD}" -gt "${NEXT_EVENT[1]}" ]; then
-         break
-      elif [ "${I_JD}" -eq "${NEXT_EVENT[1]}" ]; then
-         if [ "${EVENT_H}" -gt "${NEXT_EVENT[2]}" ]; then
-            break
-         elif [ "${EVENT_H}" -eq "${NEXT_EVENT[2]}" ]; then
-            if [ "${EVENT_M}" -gt "${NEXT_EVENT[3]}" ]; then
-               break
-            fi
-         fi
-      fi
-      NEXT_EVENT=( ${cronline[0]} ${I_JD} ${EVENT_H} ${EVENT_M} )
 
-      break
+
+            if [ "${I_JD}" -eq "${NOW_JD}" -a "${EVENT_H}" -eq "${NOW_TIME_H}" ]; then
+               if [ "${cronline[1]}" = "*" ]; then
+                  EVENT_M=${NOW_TIME_M}
+               else
+                  is_le ${NOW_TIME_M} ${cronline[1]}
+                  if [ $? -eq 0 ]; then
+                     [[ "${NOW_TIME_H}" -eq "23" ]] && continue
+                     if [ "${cronline[2]}" = "*" ]; then
+                        EVENT_H=$((NOW_TIME_H+1))
+                     else
+                        is_le $((NOW_TIME_H+1)) ${cronline[2]}
+                        [[ $? -eq 0 ]] && continue
+                        EVENT_H=`get_le $((NOW_TIME_H+1)) ${cronline[2]}`
+                     fi
+                     EVENT_M=`min ${cronline[1]}`
+                  else
+                     EVENT_M=`get_le ${NOW_TIME_M} ${cronline[1]}`
+                  fi
+               fi
+            else
+               if [ "${cronline[1]}" = "*" ]; then
+                  EVENT_M=0
+               else
+                  EVENT_M=`min ${cronline[1]}`
+               fi
+            fi
+
+            if [ "${I_JD}" -gt "${NEXT_EVENT[1]}" ]; then
+               break 1
+            elif [ "${I_JD}" -eq "${NEXT_EVENT[1]}" ]; then
+               if [ "${EVENT_H}" -gt "${NEXT_EVENT[2]}" ]; then
+                  break 1
+               elif [ "${EVENT_H}" -eq "${NEXT_EVENT[2]}" ]; then
+                  if [ "${EVENT_M}" -gt "${NEXT_EVENT[3]}" ]; then
+                     break 1
+                  fi
+               fi
+            fi
+            NEXT_EVENT=( ${cronline[0]} ${I_JD} ${EVENT_H} ${EVENT_M} )
+
+            break 1
+         done
+
+      IFS=$PIFS
       
    done
 }
@@ -321,53 +381,54 @@ function next_event()
 #
 # ZERO_JD=2454850 # Sunday, January 18, 2009
 #
-SCHEDULE_FILE=$1
-NOW_DATE=`date +'%-Y %-m %-d'`  # 2009 1 18
-NOW_DATE_M=$((`date +'%-m'`))  # 1
-NOW_DATE_D=`date +'%-d'`  # 18
-NOW_DATE_W=`date +'%-u'`  # 1
-NOW_TIME=`date +'%-H %-M'`  # 23 11
-NOW_TIME_H=$((`date +'%-H'`))  # 23
-NOW_TIME_M=$((`date +'%-M'`))  # 11
-#NOW_TIME_S=$((`date +'%-S'`))  # 7
-NOW_JD=`get_astro_JD $NOW_DATE`
-NOW_WD=$((NOW_JD-NOW_JD/7*7+1)) # 1 - MON, 2 - TUE, ..., 7 - SUN
 
-LAST_EVENT=( 0 0 0 0 )
-NEXT_EVENT=( 0 3999999 23 59 )
 
+
+WORK_DIR=`dirname ${0}`
+. ${WORK_DIR}/screen.conf
+
+SCHEDULE_FILE=${MEDIA_DIR}/${1}.schedule
+
+date +"== %c - Schedule for screen ${SCREEN_ID} =="
+cat ${SCHEDULE_FILE}
 
 set -f  # disable globbing. wildcards would be expanded otherwise
-OIFS=$IFS; IFS=';'
 
-firstline=1
+OIFS=$IFS; IFS=' ;'
+   last_event < ${SCHEDULE_FILE}
+IFS=$OIFS
+echo "= First event ="
+date +"%c Start collection ${LAST_EVENT[0]} for Julian Date ${LAST_EVENT[1]}, ${LAST_EVENT[2]}:${LAST_EVENT[3]}."
+#echo "${WORK_DIR}/collection.player.sh ${LAST_EVENT[0]} &"
+${WORK_DIR}/collection.player.sh ${LAST_EVENT[0]} &
+old_cpid=${!}
 
-while read l
+echo "= Events ="
+while [ : ]
 do
-   if [ $firstline -eq 1 ]
-   then
-      firstline=0
-      continue
-   fi
-   echo $l
+   OIFS=$IFS; IFS=' ;'
+      next_event < ${SCHEDULE_FILE}
+   IFS=$OIFS
 
-   cronline=( $l ) # split $l by semicolon
-   
-   cronline[1]=`echo ${cronline[1]} | tr ',' ' '`
-   cronline[2]=`echo ${cronline[2]} | tr ',' ' '`
-   cronline[3]=`echo ${cronline[3]} | tr ',' ' '`
-   cronline[4]=`echo ${cronline[4]} | tr ',' ' '`
-   cronline[5]=`echo ${cronline[5]} | tr ',' ' '`
-   SIFS=$IFS; IFS='-'
-      cronline[8]=`get_astro_JD ${cronline[6]}`
-      cronline[9]=`get_astro_JD ${cronline[7]}`
-   IFS=$SIFS; 
+   NOW_S=$((NOW_JD*3600*24+`date +'%-H'`*3600+`date +'%-M'`*60+`date +'%-S'`))
+   NXT_S=$((NEXT_EVENT[1]*3600*24+NEXT_EVENT[2]*3600+NEXT_EVENT[3]*60))
+   D=$((NXT_S-NOW_S))
 
-   PIFS=$IFS; IFS=" ,"
-      last_event
-      next_event
-   IFS=$PIFS
+#   echo "Sleep for $D seconds"
+   sleep $D
    
+   date +"%c Start collection ${NEXT_EVENT[0]} for Julian Date ${NEXT_EVENT[1]}, ${NEXT_EVENT[2]}:${NEXT_EVENT[3]}."
+   # Discard output from collection.player.sh
+   ${WORK_DIR}/collection.player.sh ${LAST_EVENT[0]} &>/dev/null &
+   new_cpid=${!}
+   ( pstree -Alpa $old_cpid | cut -d, -f2 | cut -d' ' -f1 |  while read ps; do kill $ps; done ) &
+   old_cpid=$new_cpid
+done
+
+echo "We should never reach here."
+exit 0
+
+
   # echo '0 id           :'${cronline[0]}  # id
   # echo '1 minutes      :'${cronline[1]}  # minutes
   # echo '2 hours        :'${cronline[2]}  # hours
@@ -378,12 +439,3 @@ do
   # echo '7 valid to     :'${cronline[7]}  # valid to  
   # echo '8 JD valid from:'${cronline[8]}  # JD valid from
   # echo '9 JD valid to  :'${cronline[9]}  # JD valid to  
-
-done < ${SCHEDULE_FILE}
-
-IFS=$OIFS
-echo "start: ${NOW_JD} ${NOW_TIME}"
-date +'now: %-Y %-m %-d %-H %-M %-S'
-echo "last: ${LAST_EVENT[*]}"
-echo "next: ${NEXT_EVENT[*]}"
-
