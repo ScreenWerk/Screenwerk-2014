80a0eaa1a0fdc0dabc1376ee7a905423d7b92332
argoroots <argo@roots.ee>
2009-04-14 19:05:53 +0000

    Et ei näitaks upload kausta.
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@150 7a690eda-67ec-4556-b061-4c86d960b817

--- www/javascripts/uploadify/upload.php
+++ www/javascripts/uploadify/upload.php
@@ -26,7 +26,7 @@ THE SOFTWARE.
 if (!empty($_FILES)) {
 	$tempFile = $_FILES['Filedata']['tmp_name'];
 	$targetPath = $_SERVER['DOCUMENT_ROOT'] . $_GET['folder'] . '/';
-	$targetFile =  str_replace('//','/',$targetPath) . $_FILES['Filedata']['name'];
+	$targetFile =  str_replace('//','/../ftp/incoming/',$targetPath) . $_FILES['Filedata']['name'] .'/';
 	
 	// Uncomment the following line if you want to make the directory if it doesn't exist
 	// mkdir(str_replace('//','/',$targetPath), 0755, true);
