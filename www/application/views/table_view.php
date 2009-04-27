2e422b3b73160af6dca1bda12bd10d4030612855
argoroots <argo@roots.ee>
2009-04-27 10:52:39 +0000

    Edit link nähtavaks.
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@175 7a690eda-67ec-4556-b061-4c86d960b817

--- www/application/views/table_view.php
+++ www/application/views/table_view.php
@@ -1,23 +1,17 @@
-<h2><?= humanize(plural($this->router->class)); ?></h2>
-
 <table class="data_table">
 
 <?php 
 	$header_set = FALSE;
 	foreach($data as $row_id => $row_value):
 		if(!$header_set) {
-			echo '<tr><th>'. implode('</th><th>', array_keys($row_value)) .'</th></tr>';
+			echo '<tr><th>'. implode('</th><th>', array_keys($row_value)) .'</th><th>&nbsp;</th></tr>';
 			$header_set=TRUE;
 		}
 ?>
 
 	<tr>
 		<td><?= implode('</td><td>', array_values($row_value)); ?></td>
-<?php
-	if(isset($_SESSION['forms'][$this->router->class .'/edit'])) {
-		echo '<td>'. anchor(site_url($this->router->class .'/edit/'.  $row_id), 'Edit') .'</td>';
-	}
-?>
+		<td><?= anchor(site_url($this->router->class .'/edit/'.  $row_id), 'Edit'); ?></td>
 	</tr>
 
 <?php endforeach; ?>
