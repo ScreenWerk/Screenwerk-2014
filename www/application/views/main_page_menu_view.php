a4cb41d6623643aca12352556d19eeb57564012b
argoroots <argo@roots.ee>
2009-04-27 09:14:02 +0000

    Fixes #96
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@172 7a690eda-67ec-4556-b061-4c86d960b817

--- www/application/views/main_page_menu_view.php
+++ www/application/views/main_page_menu_view.php
@@ -2,8 +2,7 @@
 <?php
 	if(isset($_SESSION['menu'])) {
 		foreach($_SESSION['menu'] as $key => $value):
-			if(($key == $this->router->class .'/'. $this->router->method) 
-			 || ($key == $this->router->class .'/index')) {
+			if($key == $page_menu_code) {
 				echo '<li><span id="menu_current_l">&nbsp;</span><span id="menu_current">'. $value .'</span><span id="menu_current_r"/>&nbsp;</span></li>';
 			} else {
 				echo '<li>'. anchor(site_url($key), $value) .'</li>';
