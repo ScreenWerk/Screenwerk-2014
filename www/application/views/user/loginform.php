0ebb43fc774c62c5a013fb03e9d240aec141fd26
argoroots <argo@roots.ee>
2009-04-28 07:13:02 +0000

    Fixes #57
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@185 7a690eda-67ec-4556-b061-4c86d960b817

--- www/application/views/user/loginform.php
+++ www/application/views/user/loginform.php
@@ -1,7 +1,13 @@
-<?= form_open(site_url('user/login'),  array('style' => 'margin-top:20px;')); ?>
+<?= form_open(site_url('user/login'),  array('style'=>'margin-top:20px;')); ?>
 <?= form_label('Username', 'user_name'); ?>
-<?= form_input(array('name'=>'user_name')); ?><br />
+<?= form_input(array('id'=>'user_name', 'name'=>'user_name')); ?><br />
 <?= form_label('Password', 'user_secret'); ?>
 <?= form_password(array('name'=>'user_secret')); ?><br />
 <?= form_label('&nbsp;', 'login'); ?>
 <?= form_submit('login', 'LogIn'); ?>
+
+<script type="text/javascript">
+	window.onload = function() {
+		document.getElementById('user_name').focus();	
+	}
+</script>
