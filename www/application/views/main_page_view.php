c53715376132eb65ac1997c27ff94041f491555b
argoroots <argo@roots.ee>
2009-04-30 07:29:41 +0000

    Google Analytics
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@221 7a690eda-67ec-4556-b061-4c86d960b817

--- www/application/views/main_page_view.php
+++ www/application/views/main_page_view.php
@@ -61,5 +61,17 @@
 				<a href="http://validator.w3.org/check?uri=referer">2009</a>
 			</div>
 		</div>
+
+		<script type="text/javascript">
+			var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+			document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script>
+		<script type="text/javascript">
+			try {
+				var pageTracker = _gat._getTracker("UA-260765-12");
+				pageTracker._trackPageview();
+			} catch(err) {}
+		</script>
+
 	</body>
 </html>
