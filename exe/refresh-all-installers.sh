81f59e20fb1dca74e771310ff8d1009344f3b9ca
mitselek <mihkel.putrinsh@gmail.com>
2009-05-26 11:28:23 +0000

    Resolves #136. Player 0.3.2
    git-svn-id: http://argo.unfuddle.com/svn/argo_screenwerk/trunk@296 7a690eda-67ec-4556-b061-4c86d960b817

--- exe/refresh-all-installers.sh
+++ exe/refresh-all-installers.sh
@@ -3,10 +3,15 @@
 
 wget http://www.screenwerk.eu/player/get_player_md5_list
 
+rm -R /swtrunk/ftp/players/SWPlayer
+
+unzip /swtrunk/ftp/players/SWPlayer_${1}.airi -d /swtrunk/ftp/players/SWPlayer
+
+
 cat get_player_md5_list | \
 while read md5
 do
-	/swtrunk/exe/sign-player-installer.sh $md5 &
+	/swtrunk/exe/sign-player-installer.sh $md5 
 done
 
 rm get_player_md5_list
