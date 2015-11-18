4ee5c3abf7b4385b9c5f4361b6772d36edc82d4a
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-11-18 14:46:04 +0200

    get latest release before every launch (win)

--- latest.bat
+++
@@ -1,6 +0,0 @@
-git fetch --tags
-for /f "delims=" %%a in ('git rev-list --tags --max-count=1') do @SET rev=%%a
-ECHO %rev%
-for /f "delims=" %%a in ('git describe --tags %rev%') do @SET tag=%%a
-ECHO %tag%
-git checkout %tag%
--- launcher.bat
+++ launcher.bat
@@ -1,2 +1,7 @@
-git pull
+git fetch --tags
+for /f "delims=" %%a in ('git rev-list --tags --max-count=1') do @SET rev=%%a
+for /f "delims=" %%a in ('git describe --tags %rev%') do @SET tag=%%a
+ECHO Running on release %tag% (commit %rev%)
+git checkout %tag%
+npm update
 nwjs .
