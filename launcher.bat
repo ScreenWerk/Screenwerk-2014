00713adc20810d8e517ceeb4aff353a3b9345230
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-11-18 15:06:34 +0200

    test

--- launcher.bat
+++ launcher.bat
@@ -3,7 +3,7 @@ for /f "delims=" %%a in ('git rev-list --tags --max-count=1') do @SET rev=%%a
 for /f "delims=" %%a in ('git describe --tags %rev%') do @SET tag=%%a
 ECHO Running on release %tag% (commit %rev%)
 git checkout --force %tag%
-npm update
+(npm update)
 ECHO Starting ScreenWerk
 nwjs
 ECHO uzredzēšanos!
