a11b3b75f3584422cee1479430c478c9499bc13f
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-11-18 14:38:24 +0200

    test

--- latest.bat
+++ latest.bat
@@ -1,4 +1,6 @@
 git fetch --tags
-for /f "delims=" %%a in ('git rev-list --tags --max-count=1') do @set rev=%%a
-for /f "delims=" %%a in ('git describe --tags %%rev%%') do @set tag=%%a
-git checkout %%tag%%
+for /f "delims=" %%a in ('git rev-list --tags --max-count=1') do @SET rev=%%a
+ECHO %rev%
+for /f "delims=" %%a in ('git describe --tags %rev%') do @SET tag=%%a
+ECHO %tag%
+git checkout %tag%
