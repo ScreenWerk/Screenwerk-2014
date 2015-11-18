6b4a77bc4c90abe8cdfb5e94c97b7bb9ce9bbe99
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-11-18 14:58:02 +0200

    test

--- launcher.bat
+++ launcher.bat
@@ -2,7 +2,7 @@ git fetch --tags
 for /f "delims=" %%a in ('git rev-list --tags --max-count=1') do @SET rev=%%a
 for /f "delims=" %%a in ('git describe --tags %rev%') do @SET tag=%%a
 ECHO Running on release %tag% (commit %rev%)
-git checkout %tag%
+git checkout --force %tag%
 npm update
 nwjs .
 ECHO uzredzēšanos!
