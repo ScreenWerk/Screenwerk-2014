31378e881fc8c506ec675b774629056a81489f18
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-11-24 09:55:32 +0200

    added launcher for running on latest commit to master branch

---
+++ latest.bat
@@ -0,0 +1,5 @@
+git checkout --force master
+call npm update
+ECHO Enjoy latest ScreenWerk (beware of bugs)
+call nwjs
+ECHO Exited ScreenWerk
--- launcher.bat
+++ launcher.bat
@@ -3,6 +3,6 @@ for /f "delims=" %%a in ('git rev-list --tags --max-count=1') do @SET rev=%%a
 for /f "delims=" %%a in ('git describe --tags %rev%') do @SET tag=%%a
 git checkout --force %tag%
 call npm update
-ECHO Starting ScreenWerk
+ECHO Enjoy ScreenWerk
 call nwjs
 ECHO Exited ScreenWerk
