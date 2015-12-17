dd40c505f54ca27df1589b7efb41969347a9f5f0
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-12-17 17:05:36 +0200

    win32

--- code/slackbots.js
+++ code/slackbots.js
@@ -112,10 +112,10 @@ function latest() {
             }
             console.log('== win32: latest.bat succeeded')
             slackbot.postMessageToChannel('test', datestring + ':*' + c.__SCREEN_ID + '*: :exclamation: updated to latest build.', {as_user: true})
+            setTimeout(function () {
+                process.exit(0)
+            }, 1000)
         })
-        setTimeout(function () {
-            process.exit(0)
-        }, 1000)
     } else {
         slackbot.postMessageToChannel('test', datestring + ':*' + c.__SCREEN_ID + '*: :exclamation: Player doesnot now, what to do with ' + process.platform, {as_user: true})
     }
