6f641ce34448ed0530d09efad10f71649b30bde1
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-12-17 17:09:12 +0200

    win32

--- latest.bat
+++ latest.bat
@@ -2,6 +2,6 @@ git checkout --force release
 git pull
 call npm update
 @ECHO Enjoy latest ScreenWerk (beware of bugs)
-start /b nwjs
+start "SW Player 2014" /i nwjs
 @ECHO Started ScreenWerk
 exit 0
