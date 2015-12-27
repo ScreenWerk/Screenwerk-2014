869f4742860d07ba6ce7f0d83ee5460b2e39d268
Mihkel Putrinš <mihkel.putrinsh@gmail.com>
2015-12-27 17:45:05 +0200

    fix

--- code/slackbots.js
+++ code/slackbots.js
@@ -50,7 +50,7 @@ function captureScreenshot() {
     c.player_window.capturePage(function(buffer) {
         c.log.info('Got ss')
         var datestring = new Date().toISOString().replace(/T/, ' ').replace(/:/g, '-').replace(/\..+/, '')
-        var screenshot_path = c.__LOG_DIR + '/screencapture ' + datestring + '.raw.jpeg'
+        var screenshot_path = path.join(c.__LOG_DIR, c.__SCREEN_ID + '_' + datestring + '.png'
         var writer = fs.createWriteStream(screenshot_path)
         if (writer.write(buffer) === false) {
             writer.once('drain', function() {writer.write(buffer)})
